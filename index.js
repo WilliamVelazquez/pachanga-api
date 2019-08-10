ca70ad86b92ad76271f3543e7fd294218900df5f
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-08-10 12:43:35 -0500

    Creating EndPoint for Testing and Adding Nodemon DevDependency

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('init');
+const express = require('express')
+
+const PORT = '8080'
+const app = express()
+
+app.use(express.json())
+
+app.post('/events',(request, response)=>{
+    const { name, date, items, location }= request.body
+    response.json({
+        name,
+        date,
+        items,
+        location
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running in Port ${PORT}`);
+})
