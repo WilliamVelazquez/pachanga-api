d55289a130f4b5d7347a1a6731ff73c166fb2ca3
William Velazquez <34907456+WilliamVelazquez@users.noreply.github.com>
2019-08-10 14:50:34 -0500

    Creating item and event Models

---
+++ models/event.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const itemSchema = require('./item')
+
+const eventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 5,
+        maxlength: 10,
+        required: true
+    },
+    date:{
+        type: Date,
+        required: true
+    },
+    items: {
+        type: [itemSchema]
+    },
+    location:{
+        type: String,
+        minlength: 5,
+        maxlength: 120,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Event', eventSchema)
